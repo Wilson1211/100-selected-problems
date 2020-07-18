61c99f4b66ade4f29cd091896fc49601bda43e9c
Wilson1211 <raito244@gmail.com>
2020-07-18 21:41:44 +0800

    normal push

--- BUCKET.c
+++ BUCKET.c
@@ -2,46 +2,56 @@
 #include <stdlib.h>
 
 #define   MAXSIZE   1000
+#define   LENGTH    4
+#define   BUCKET_NO 16
+#define   LOOP_COUNT sizeof(int)*8/LENGTH
+
 
 #define swap(a,b) {int t;t = a;a = b;b = t;}
-#define initnode(x) Node x = {->data=0, ->next = NULL}
+//#define initnode(x) Node x = {.data=0, .next = NULL}
 
+Node *head;
+int mask = 0x0FU;
 
-typedef struct node *Node;
+typedef struct node Node;
 
 struct node{
     int data;
-    Node next;
+    int x;
+    Node *next;
 };
 
-void sort(int x[], int n){
-    Node a[40], head, end, tmp;
-    int i, j;
-    int l = 10, r = 1;
-    for(i=0;i<n;i++){
-        Node y = malloc(sizeof(struct node));
-        j = x[i]%l;
-        j = x[i]/r;
-        y->data = x[i];
-        if(a[j]->next == NULL){
-            a[j]->next = y;
-        }else{
-            tmp = a[j]->next;
-            while(tmp->next != NULL) {tmp = tmp->next;}
-            tmp->next = y;
-        }
-    }
+struct cell{
+    Node *first;
+    Node *last;
+}basket[BUCKET_NO];
 
-    // find head
-    i=0;
-    while(a[i]->next == NULL) i++;
-    head->next = a[i]->next;
-    tmp = head->next;
+struct Node* listgen(int[], int);
+void distribute(void);
+void recollect(void);
+void putback(int []);
 
+
+struck Node* listgen(int x[], int n){
+    Node *first, *last, *tmp;
+    int i;
+    
+    first = last = (Node*) malloc(sizeof(Node));
     for(i=0;i<n;i++){
-        tmp->next = a[i]->next;
-        while(tmp->next != NULL) tmp = tmp->next;        
+        tmp = (Node*) malloc(sizeof(Node));
+        tmp.data = input[i];
+        tmp.next = NULL;
+        last.next = tmp;
+        last = tmp;
     }
+    return first;
+}
+
+
+void sort(int input[], int n){
+
+
+
 
 }
 
@@ -74,3 +84,66 @@ void main(void)
      }
 }
 
+
+
+/*
+void initNodeArray(Node a[], int n){
+    int i;
+    for(i=0;i<n;i++){
+        a[i].data = 0;
+        a[i].next = NULL;
+    }
+}
+
+void find_max(int x[], int n, int *max, int *length){
+    int i, tmp;
+    *length = 0;
+    *max = x[0];
+    for(i=1;i<n;i++){
+        *max = (*max>x[i])?*max;x[i];
+    }
+    tmp = *max;
+    while(tmp > 0){
+        *length++;
+        tmp/=10;
+    }
+}
+
+void sort(int x[], int n){
+    Node a[40], head, end, tmp;
+    int max, length; // for length of number
+    int i, j, count;
+    int l = 10, r = 1;
+    find_max(x, n, max, length);
+    initNodeArray(a);
+    while(length){
+        for(i=0;i<n;i++){
+            //Node y = malloc(sizeof(struct node));
+            initnode(y);
+            j = x[i]%l;
+            j = j/r;
+            y.data = x[i];
+            if(a[j].next == NULL){
+                a[j].next = &y;
+            }else{
+                tmp = *(a[j].next);
+                count = 0;
+                while(tmp.next != NULL) {tmp = *(tmp.next);count++;}
+                tmp.next = &y;
+            }
+        }
+
+        // find head
+        i=0;
+        while(a[i].next == NULL) i++;
+        head.next = a[i].next;
+        tmp = *(head.next);
+
+        for(i=0;i<n;i++){
+            tmp.next = a[i].next;
+            while(tmp.next != NULL) tmp = *(tmp.next);        
+        }
+        length--;
+    }
+}
+*/
