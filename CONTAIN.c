8dabbda07c47c3956614c2cb4fe504531f2535fb
Wilson1211 <raito244@gmail.com>
2020-07-30 00:47:42 +0800

    normal push

--- CONTAIN.c
+++ CONTAIN.c
@@ -34,9 +34,33 @@ void sort(int left[], int right[], int n){
 }
 
 void qsort(int right[], int left[], int first, int last){
-    int 
-    split()
+    if(first >= last) return;
+    int split_num;
+    split(left, right, first, last, &split_num);
+    qsort(left, right, first, split_num-1);
+    qsort(left, right, split_num+1, last);
 }
 
 
-void split(int )
+void split(int left[], int right[], int first, int last, int *split_num){
+    int i, j, k;
+    int pivot, pivot_num;
+    pivot = first;
+    pivot_num = left[first];
+    for(i=first+1;i<=last;i++){
+        if(left[i] < pivot_num){
+            pivot++;
+            swap(left[i], left[pivot]);
+            swap(right[i], right[pivot]);
+        }
+    }
+    swap(left[first], left[pivot]);
+    swap(right[first], right[pivot]);
+}
+
+void swap(int *a, int *b){
+    int t;
+    t = *a;
+    *a = *b;
+    *b = t;
+}
--- MAXVISIT.c
+++ MAXVISIT.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 //#include <math.h>
 
-// this problem only needs to count the maximum people 
+// this problem only needs to count the maximum people in lobby 
+// at the same time
 
 #define   MAXSIZE   1000
 #define   IN        1
@@ -19,3 +20,23 @@ void sort(TABLE [], int);
 void qsort(TABLE [], int, int);
 void split(TABLE [], int, int, int *);
 void swap(TABLE *, TABLE *);
+
+
+void maxvisit(int left[], int right[], int n){
+    int i, j, k;
+    
+    
+    TABLE table = malloc(sizeof(table)*2*n);
+    
+    for(i=0;i<n;i++){
+        table[i]->clock = left[i];
+        table[i]->status = IN;
+        table[i+n]->clock = right[i];
+        table[i+n]->status = OUT;
+    }
+
+    sort(table, 2*n);
+}
+
+
+
