0940cd25b4f0749642552e95ef3746e14a7492bd
Wilson1211 <raito244@gmail.com>
2020-09-18 00:59:13 +0800

    normal push

--- KNAPSACK.c
+++ KNAPSACK.c
@@ -6,10 +6,25 @@
 #define N 10
 #define MIN(a, b) ((a>b)?b:a)
 
+
+void knapsack(int pack[], int n, int p, int K) {
+
+    
+
+}
+
 int main() {
 
+    int i, j, k, n, p, K;
+    int *pack;
+    scanf("%d", &n);
+    scanf("%d", &p);
+    scanf("%d", &K);
+    pack = (int *)malloc(sizeof(int)*n);
+    for(int i=0;i<n;i++) {
+        scanf("%d", pack[i]);
+    }
 
-
-
+    free(pack);
     return 0;
 }
